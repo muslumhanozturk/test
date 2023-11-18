03b34d6923f86477b733c7b560605fdb89086e1e
M.Han OZTURK <121254861+muslumhanozturk@users.noreply.github.com>
2023-11-18 22:17:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ from flask.ext.bcrypt import Bcrypt
 # Create and name Flask app
 app = Flask("FlaskLoginApp")
 
-# database connection
+
 app.config['MONGODB_SETTINGS'] = {'HOST':os.environ.get('MONGOLAB_URI'),'DB': 'FlaskLogin'}
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.debug = os.environ.get('DEBUG',False)
 
-db = MongoEngine(app) # connect MongoEngine with Flask App
-app.session_interface = MongoEngineSessionInterface(db) # sessions w/ mongoengine
+db = MongoEngine(app) 
+app.session_interface = MongoEngineSessionInterface(db) 
 
-# Flask BCrypt will be used to salt the user password
+
 flask_bcrypt = Bcrypt(app)
 
-# Associate Flask-Login manager with current app
+
 login_manager = LoginManager()
 login_manager.init_app(app)
